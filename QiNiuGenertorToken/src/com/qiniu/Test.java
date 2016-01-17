13c723d0a6fc539489ad21f518e10e85490f2683
Chars Davy <chars_d@126.com>
2016-01-17 16:34:40 +0800

    添加代码

---
+++ QiNiuGenertorToken/src/com/qiniu/Test.java
@@ -0,0 +1,15 @@
+package com.qiniu;
+
+import com.qiniu.util.Auth;
+
+public class Test {
+
+    public static void main(String[] args) {  
+    	//前一个参数是从七牛网站上得到的AccessKey,后一个参数是SecretKey
+        Auth auth=Auth.create("sfPKD6WfFblx2v9I1CgI1A9ALzdpXDpGZzz2MSeI", "TWCtZRkgeqAX0xIUs0aCZhFMEslrrw0WhIIVCmK6");
+        //第一个参数是你建立的空间名称，第三个是时间长度(按毫秒算的，我这里写的是10年)
+        System.out.println("the token is:");
+        String token=auth.uploadToken("mynotes", null, 1000*3600*24*12*10, null);
+        System.out.println(token);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # QiNiuGenertorToken
 七牛云存储服务器token获取
+
+#method
+* 七牛官网创建用户账号；</br>
+* 创建应用空间；</br>
+* 获取Access Key；</br>
+* 获取Secret Key；</br>
+* 填充对应参数；</br>
+* 获得token。
+
+#blog
+my blog is [here](http://my.oschina.net/chars/blog), welcome to visit.
+
+#e-mail
+chars_d@126.com
