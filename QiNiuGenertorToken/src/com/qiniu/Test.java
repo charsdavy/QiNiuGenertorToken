a6b6449f528cfa5baef9e991b1a76a05f8141c25
deng wei <chars_d@126.com>
2016-01-29 09:32:01 +0800

    Update Test.java

--- QiNiuGenertorToken/src/com/qiniu/Test.java
+++ QiNiuGenertorToken/src/com/qiniu/Test.java
@@ -9,7 +9,7 @@ public class Test {
         Auth auth=Auth.create("sfPKD6WfFblx2v9I1CgIXDpGZzz2MSeI", "TWCtZRkgeqAX0Eslrrw0WhIIVCmK6");
         //第一个参数是你建立的空间名称，第三个是时间长度(按毫秒算的，我这里写的是10年)
         System.out.println("the token is:");
-        String token=auth.uploadToken("mynotes", null, 1000*3600*24*12*10, null);
+        String token=auth.uploadToken("mynotes", null, 1000*3600*24*365*10, null);
         System.out.println(token);
     }
 }
